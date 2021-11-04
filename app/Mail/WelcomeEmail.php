3753c4310b63b84b2e55be5853b25c48da88d7b4
Iffi <irfan.elahi187@gmail.com>
2021-11-04 15:45:39 +0500

    build and src

--- app/Mail/WelcomeEmail.php
+++ app/Mail/WelcomeEmail.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class ReminderEmail extends Mailable
+class WelcomeEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
